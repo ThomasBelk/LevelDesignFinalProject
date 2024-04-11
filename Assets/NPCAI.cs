e5bdfe75e2f917e3263551bda2f76d958f612001
ThomasBelk <122413223+ThomasBelk@users.noreply.github.com>
2024-04-11 14:55:35 -0400

    NPC stuff

--- Assets/NPCAI.cs
+++ Assets/NPCAI.cs
@@ -7,7 +7,11 @@ using UnityEngine.AI;
 public class NPCAI : MonoBehaviour
 {
     [SerializeField] private GameObject[] wanderPoints;
+    [SerializeField] private Transform talkPoint;
+    [SerializeField] private GameObject player;
+    private Animator anim; 
     private NavMeshAgent agent;
+    private bool hasTalked = false;
 
     // Start i called before the first frame update
     void Start()
@@ -17,12 +21,52 @@ public class NPCAI : MonoBehaviour
             wanderPoints = GameObject.FindGameObjectsWithTag("WanderPoint");
         }
 
+        if (player == null)
+        {
+            player = GameObject.FindGameObjectWithTag("Player");
+        }
+
+        anim = GetComponent<Animator>();
+        anim.SetInteger("animState", 1);
         agent = GetComponent<NavMeshAgent>();
-        agent.SetDestination(wanderPoints[0].transform.position);
+        
+        agent.SetDestination(talkPoint.position);
     }
 
     private void Update()
     {
-        
+        if (Vector3.Distance(transform.position, talkPoint.position) <= agent.stoppingDistance + .1f)
+        {
+            agent.speed = 0f;
+            LookAt(player.transform.position);
+            agent.SetDestination(transform.position);
+            anim.SetInteger("animState", 0);
+            if (!hasTalked)
+            {
+                anim.SetTrigger("Talk");
+                hasTalked = true;
+            }
+            // if (anim.GetInteger("animState") != 2 && anim.GetInteger("animState") != 0)
+            // {
+            //     anim.SetInteger("animState", 2);
+            // }
+            // else
+            // {
+            //     anim.SetInteger("animState", 0);
+            // }
+        }
+    }
+    
+    private void LookAt(Vector3 target) 
+    {
+        Vector3 direction = (target - transform.position).normalized;
+        direction.y = 0;
+        Quaternion lookRotation = Quaternion.LookRotation(direction);
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, 10 * Time.deltaTime);
+    }
+
+    public void ReturnToIdle()
+    {
+        anim.SetInteger("animState", 0);
     }
 }
