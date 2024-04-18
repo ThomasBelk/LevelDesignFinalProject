048b2d2f667bd93850bbdbba0dd58190a3f4db82
ThomasBelk <122413223+ThomasBelk@users.noreply.github.com>
2024-04-18 14:16:09 -0400

    Light flicker script

---
+++ Assets/FlickeringLight.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Timers;
+using UnityEngine;
+
+public class FlickeringLight : MonoBehaviour
+{
+    public Light light;
+    public float minTime;
+    public float maxTime;
+    private float timer;
+
+    public Material offMat;
+    public Material onMat;
+
+    //public AudioSource audio;
+    private MeshRenderer meshRend;
+    public int materialsIndex = 1;
+    
+    void Start()
+    {
+        timer = Random.Range(minTime, maxTime);
+        meshRend = GetComponent<MeshRenderer>();
+    }
+
+    void Update()
+    {
+        Flicker();
+    }
+
+    private void Flicker()
+    {
+        if (timer > 0)
+        {
+            timer -= Time.deltaTime;
+        }
+
+        if (timer <= 0)
+        {
+            light.enabled = !light.enabled;
+            if (light.enabled)
+            {
+                var copy = meshRend.materials;
+                copy[materialsIndex] = onMat;
+                meshRend.materials = copy;
+            }
+            else
+            {
+                var copy = meshRend.materials;
+                copy[materialsIndex] = offMat;
+                meshRend.materials = copy;
+            }
+            timer = Random.Range(minTime, maxTime);
+            
+        }
+    }
+}
